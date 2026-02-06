2631ca876d63ec6826de0c17cba6eb9c85f7ba34
Nando2003 <nandofontes27@gmail.com>
2026-02-06 12:05:18 -0300

    feat: adding tasks

---
+++ db/migrate/20260206141844_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[8.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.datetime :due_date
+      t.integer :priority
+      t.boolean :completed, default: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
